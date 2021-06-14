e408816fdd410a2e36c80bfb3d2abd92289d7d58
FlashBangKev <kevin@getkev.co.uk>
2021-06-14 10:06:40 +0100

    started javascript, selected dom elements

---
+++ script.js
@@ -0,0 +1,30 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+
+// Play & Pause Video
+
+const toggleVideoStatus = () => {
+    return true;
+}
+
+
+// update play pause icon
+const updatePlayIcon = () => {
+    return true;
+}
+
+
+// Event Listenders
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', togglePlayIcon);
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
